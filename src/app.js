45a8b1933dd41a62cb93dbdac263969d92f24144
suraj kumar <surajkr8281@gmail.com>
2024-08-06 20:09:52 +0530

    added more routes and controllers

--- src/app.js
+++ src/app.js
@@ -23,14 +23,31 @@ app.get('/', function (req,res){
 
 
 // router import
-import userRoutes from "./routes/user.route.js"
+import userRouter from "./routes/user.route.js"
+import healthcheckRouter from "./routes/healthcheck.route.js"
+import tweetRouter from "./routes/tweet.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
+import videoRouter from "./routes/video.route.js"
+import commentRouter from "./routes/comment.route.js"
+import likeRouter from "./routes/like.route.js"
+import playlistRouter from "./routes/playlist.route.js"
+import dashboardRouter from "./routes/dashboard.route.js"
 
 
 
 // router declaration
-app.use("/api/v1/users" , userRoutes);
+app.use("/api/v1/users" , userRouter);
+app.use("/api/v1/healthcheck" , healthcheckRouter);
+app.use("/api/v1/tweets" , tweetRouter);
+app.use("/api/v1/subscriptions" , subscriptionRouter);
+app.use("/api/v1/videos" , videoRouter);
+app.use("/api/v1/comments" , commentRouter);
+app.use("/api/v1/likes" , likeRouter);
+app.use("/api/v1/playlist" , playlistRouter);
+app.use("/api/v1/dashboard" , dashboardRouter);
 
 
+// http://localhost:8000/api/v1/users/register
 
 
 export { app }
